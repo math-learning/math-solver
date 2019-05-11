6ebf900f751f6f211d27c928d10db347d2dc5bc6
Diego Kim <diego.kim@despegar.com>
2019-05-11 13:44:44 -0300

    list utils logic to match diff size lists

--- src/utils/list_utils.py
+++ src/utils/list_utils.py
@@ -45,7 +45,7 @@ class ListUtils:
             larger = listtwo
             smaller = listone
 
-        return ListUtils.combinations_diferent_sizes_rec(larger, smaller)
+        return ListUtils.combinations_diferent_sizes_rec(larger, smaller, [])
 
     # Idea:
     # A = [a,b] ; B = [1,2,3,4]
@@ -57,22 +57,87 @@ class ListUtils:
     @staticmethod
     def combinations_diferent_sizes_rec(larger, smaller, possibilities):
         diff = len(larger) - len(smaller)
-        
+
         # Each item is an array of numbers representing the amount of items of that length joined
         # for example with a difference in length of 4 and the length of the smaller 2
         # [0,2,0,0] a possible reduction is 0 elements of lenght 1; two elements of length 2; etc..
-        join_possibilities = possibilities
+        join_possibilities = ListUtils.get_list_of_joins(len(smaller), len(larger))
         
+        for join_case in join_possibilities:
+            
+            for quantity in join_case:
+                amount_joined = join_case.index(quantity) + 1
+
+                for i in range(0, quantity):
+                    combinations = ListUtils.combinations_of_n_elem(larger, amount_joined)
+
+
+                    
+
+
+    # TODO: refactor
+    @staticmethod
+    def calculate_rec(join_case, current_index, quantity, rest, accum, total):
+
+        if len(join_case) < current_index + 1:
+            return [accum]
+
+        if quantity == 0:
+            new_index = current_index + 1
+            new_quantity = 0
+            if len(join_case) > current_index + 1:
+                new_quantity = join_case[current_index + 1] 
+            return ListUtils.calculate_rec(join_case, new_index, new_quantity, rest, accum, total)
+
+        amount_joined = current_index + 1
+
+        combinations = ListUtils.combinations_of_n_elem(rest, amount_joined)
+        for combination in combinations:
+            accum_copy = accum[:]
+            accum_copy.append(combination.elements)
+            total += ListUtils.calculate_rec(join_case, current_index, quantity - 1, combination.rest, accum_copy, [])
+        
+        return total
+
+    @staticmethod
+    def combinations_of_n_elem(elements, n):
+        return ListUtils.combinations_of_n_elem_rec(elements, elements,n,[],[])
+
+    @staticmethod
+    def combinations_of_n_elem_rec(elements, all_elements, n, accum, total):
+        
+        if n == 0:
+            rest = []
+            for elem in all_elements:
+                if elem not in accum:
+                    rest.append(elem)
+            combination = SubCombination(accum, rest) 
+            return [combination]
+
+        for i in range(0, len(elements)):
+            accum_copy = accum[:]
+            elements_copy = elements[:]
+            element = elements[i]
+            for j in range(0, i + 1):
+                elements_copy.remove(elements[j])
+            accum_copy.append(element)
+            total += ListUtils.combinations_of_n_elem_rec(elements_copy, all_elements, n-1, accum_copy, [])
+
+        return total
+
+        
+
             
     # TODO: logic
     @staticmethod
     def get_list_of_joins(smaller_size, larger_size):
-        pass
+        diff = larger_size - smaller_size
+        return ListUtils.list_of_joins_rec(diff + 1, larger_size, smaller_size, [], [])
     
 
     # TODO: refactor
     @staticmethod
-    def compute_rec(amount_joined, rest_size, expected_amount, accum, total):
+    def list_of_joins_rec(amount_joined, rest_size, expected_amount, accum, total):
         if amount_joined == 0:
             sum = 0
             for item in accum:
@@ -84,13 +149,18 @@ class ListUtils:
         if (rest_size < amount_joined ):
             accum_copy = accum[:]
             accum_copy.append(0)
-            return ListUtils.compute_rec(amount_joined - 1, rest_size, expected_amount, accum_copy, total)
+            return ListUtils.list_of_joins_rec(amount_joined - 1, rest_size, expected_amount, accum_copy, total)
 
         max_amount_of_joins = int (rest_size / amount_joined)
         for i in range(0, max_amount_of_joins + 1):
             accum_copy = accum[:]
             accum_copy.append(i)
             
-            total += ListUtils.compute_rec(amount_joined - 1, rest_size - (i * amount_joined), expected_amount, accum_copy, [])
+            total += ListUtils.list_of_joins_rec(amount_joined - 1, rest_size - (i * amount_joined), expected_amount, accum_copy, [])
         
         return total
+
+class SubCombination:
+        def __init__(self, elements, rest):
+            self.elements = elements
+            self.rest = rest
--- test/utils/test_list_utils.py
+++ test/utils/test_list_utils.py
@@ -1,5 +1,5 @@
 import unittest
-from src.utils.list_utils import ListUtils
+from src.utils.list_utils import ListUtils, SubCombination
 
 class TestListUtils(unittest.TestCase):
 
@@ -43,8 +43,34 @@ class TestListUtils(unittest.TestCase):
     def test_get_list_of_joins(self):
         smaller_size = 2
         larger_size = 4
-        a = ListUtils.compute_rec(3, 4, 2,[],[])
+        a = ListUtils.get_list_of_joins(smaller_size, larger_size)
         expected = [[0,2,0], [1,0,1]]
         self.assertEquals(expected, a)
 
-    
+
+    def test_combinations_of_n_elem(self):
+
+        elements = [1,2,3]
+        n = 2
+
+        combinations = ListUtils.combinations_of_n_elem(elements, n)
+        expected = [
+                SubCombination([1,2],[3]),
+                SubCombination([1,3], [2]),
+                SubCombination([2,3], [1])
+            ]
+        self.assertEquals(expected, combinations)
+
+
+    def test_calculate_rec(self):
+        elements = [1,2,3,4]
+        join_case = [1,0,1]
+        current_index = 0
+        a = ListUtils.calculate_rec(join_case, current_index, join_case[current_index], elements, [], [])
+        expected = [
+            [[1], [2,3,4]],
+            [[2], [1,3,4]],
+            [[3], [1,2,4]],
+            [[4], [1,2,3]]
+        ]
+        self.assertEquals(expected, a)
