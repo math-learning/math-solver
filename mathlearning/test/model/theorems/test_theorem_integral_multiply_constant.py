2b625840d292a666f0c7a6503e5d8159f4f36ec8
diegokim <diego.f.kim@gmail.com>
2020-05-25 01:54:32 -0300

    add sum and multiply by a constant integral theorems

---
+++ mathlearning/test/model/theorems/test_theorem_integral_multiply_constant.py
@@ -0,0 +1,25 @@
+import unittest
+
+from mathlearning.model.expression import Expression
+from mathlearning.model.integral_theorems import IntegralTheorems
+
+theorem = IntegralTheorems.integral_multiply_for_constant()
+
+class TestTheoremIntegralMultiplyForConstant(unittest.TestCase):
+
+    def test_simple_multiplication_should_apply(self):
+        exp = Expression('\\int(2*x)dx')
+        expected = Expression('2*\\int(x)dx')
+        result = theorem.apply_to(exp)
+        self.assertEquals(len(result), 1)
+        self.assertTrue(expected.is_equivalent_to(result[0]))
+
+    def test_non_constant_multiplication_should_not_apply(self):
+        exp = Expression('\\int(x*x)dx')
+        result = theorem.apply_to(exp)
+        self.assertEquals(len(result), 0)
+
+    def test_sum_a_constant_should_not_apply(self):
+        exp = Expression('\\int(2 + x)dx')
+        result = theorem.apply_to(exp)
+        self.assertEquals(len(result), 0)
