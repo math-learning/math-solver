a70517d59a8c1d7eaa84c9d1acaa694238124bc3
diegokim <diego.f.kim@gmail.com>
2020-06-06 19:53:42 -0300

    compare replacing variables

--- mathlearning/mathlearning/model/solution_tree_node.py
+++ mathlearning/mathlearning/model/solution_tree_node.py
@@ -102,6 +102,13 @@ class SolutionTreeNode:
                 if current.expression.is_equivalent_to(expression) and \
                         current.expression.compare_variables(expression.variables):
                     return True
+
+                current_replaced = current.expression.replace_variables()
+                expression_replaced = expression.replace_variables()
+
+                if current_replaced.is_equivalent_to(expression_replaced):
+                    return True
+
                 for branch in current.branches:
                     to_check.append(branch)
             already_checked.add(current.expression.to_string())
